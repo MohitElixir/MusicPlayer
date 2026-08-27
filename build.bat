4006c85b4f1e552c97910fe57d62aead15d81f74
Mohit shahi <mohitshahi.12345@gmail.com>
2026-08-27 16:39:31 +0545

    Untrack AudioPlayer.exe, ignore all executables, and fix build.bat

--- build.bat
+++ build.bat
@@ -2,10 +2,10 @@
 REM Rebuild MusicPlayer as a fully self-contained exe.
 REM Requires g++ (MinGW-w64 / MSYS2) on PATH.
 
-g++ -std=c++17 -O2 -static -static-libgcc -static-libstdc++ -o MusicPlayer.exe main.cpp MusicManager.cpp Song.cpp UIManager.cpp -lwinmm
+g++ -std=c++17 -O2 -static -static-libgcc -static-libstdc++ -o AudioPlayer.exe MusicPlayer\main.cpp -lwinmm
 
 if %ERRORLEVEL% EQU 0 (
-    echo Build succeeded: MusicPlayer.exe
+    echo Build succeeded: AudioPlayer.exe
 ) else (
     echo Build failed.
 )
